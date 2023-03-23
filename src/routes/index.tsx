57e2dd999fdff5d53afb1c6a45ad4d7f6da130b5
Kilowon <shauncameron.inbox@gmail.com>
2023-03-23 12:37:39 -0400

    index

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,33 +1,20 @@
-import type { Component } from 'solid-js'
+import { Title } from 'solid-start'
+import Counter from '~/components/Counter'
 import { Image } from '@unpic/solid'
 
-const App: Component = () => {
+export default function Home() {
 	return (
-		<div>
+		<main>
+			<Title>Hello World</Title>
+			<h1>Hello world!</h1>
+			<Counter />
 			<Image
-				src="https://images.unsplash.com/photo-1617718295766-0f839c2853e7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTczfHxyYWluZm9yZXN0JTIwYmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60"
-				layout="fullWidth"
-				alt=""
-				priority
-				height={400}
-				background="auto"
-			/>
-			<Image
-				src="https://cdn.shopify.com/static/sample-images/garnished.jpeg"
+				src="https://cdn.shopify.com/static/sample-images/bath_grande_crop_center.jpeg"
 				layout="constrained"
 				width={800}
 				height={600}
-				alt="Shopify"
+				alt="A lovely bath"
 			/>
-			<Image
-				src="https://bunnyoptimizerdemo.b-cdn.net/bunny7.jpg?width=300"
-				width={400}
-				height={300}
-				layout="fixed"
-				alt="Bunny.net"
-			/>
-		</div>
+		</main>
 	)
 }
-
-export default App
